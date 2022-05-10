6eeeb05fe7b2fb4d63aec635c3f21f8042b53794
Dennis Kim <dkim334@my.bcit.ca>
2022-05-10 15:10:50 -0700

    added function to display google map based on current location. Map shows marker to represent current location. Added button to map to re-center map to current location.

---
+++ public/js/map.js
@@ -0,0 +1,56 @@
+'use strict';
+
+// google.maps.Map object
+let map;
+
+// navigator.geolocation parameter to enable high accuracy tracking
+var options = {
+  enableHighAccuracy: true
+};
+
+// displays error if navigator.geolocation catches an error
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+// displays map based on current location
+function initMap() {
+  navigator.geolocation.getCurrentPosition(function(pos){
+    location.lat = pos.coords.latitude;
+    location.long = pos.coords.longitude;
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: location.lat, lng: location.long},
+      zoom: 15
+    });
+
+    // creates a button re-center map to current location
+    const locationButton = document.createElement("button");
+    locationButton.textContent = "Return to current location";
+    locationButton.classList.add("custom-map-control-button");
+    map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(locationButton);
+    locationButton.addEventListener("click", () => {
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+        }
+      );
+    });
+
+    //creates a marker showing current location on map
+    var marker = new google.maps.Marker({
+      position: {lat: location.lat, lng: location.long},
+      map: map,
+      icon: {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: 10,
+        fillOpacity: 1,
+        strokeWeight: 2,
+        fillColor: '#5384ED',
+        strokeColor: '#ffffff',
+      },
+    });
+  }, error, options);
+}
+
+
+window.onload = initMap;
