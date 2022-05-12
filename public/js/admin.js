233acc4887fb07eef6068f5e5090b75fcec3764b
Bucksin <bucksin8@gmail.com>
2022-05-11 19:38:24 -0700

    completed the css for the admin page.

--- public/js/admin.js
+++ public/js/admin.js
@@ -2,6 +2,14 @@
 'use strict';
 window.addEventListener("load", loadUserList);
 
+let userMenu = document.getElementById("userEditMenu");
+
+function toggleEditUserMenu() {
+    if (userMenu.hidden) {
+        userMenu.hidden = false;
+    } else {userMenu.hidden = true;}
+};
+
 // loads a list of all users//
 function loadUserList () {
     const option = {
@@ -16,20 +24,45 @@ function loadUserList () {
                 let userList = document.getElementById("user-list-container");
 
                 for( let i=0; i < users.length; i++) {
-                    let newUser = document.createElement("div");
+
+                    //const imagePath = `./public/img/userAvatars/avatar-user${users[i].userID}`; 
+                    let newUser = document.createElement("button");
                     newUser.classList.add("user");
-                    newUser.innerHTML = `<div class="user-uid"></div> <div class="user-name"></div> <div class="user-displayname"></div>`;
-                    newUser.querySelector(".user-uid").innerHTML = users[i].userID;
-                    newUser.querySelector(".user-name").innerHTML = users[i].fname + " " + users[i].lname;
-                    newUser.querySelector(".user-displayname").innerHTML = users[i].displayName;
+                    newUser.value = users[i].userID;
+                    const defaultProfilePic = `<img src="/img/userAvatars/default.png" alt="profile">`;
+                    const userInfo = `<div class="user-names"><div class="user-uid"></div> <div class="user-name"></div> <div class="user-displayname"></div></div>`;
+                    newUser.innerHTML = defaultProfilePic + userInfo;
+                    newUser.querySelector(".user-uid").innerHTML = "ID: " + users[i].userID;
+                    newUser.querySelector(".user-name").innerHTML = "Name: " + users[i].fname + " " + users[i].lname;
+                    newUser.querySelector(".user-displayname").innerHTML = "User: " + users[i].displayName;
                     userList.appendChild(newUser);
+                    
                 }
 
                 let user = document.querySelectorAll(".user");
 
                 for( let i=0; i < user.length; i++ ) {
                     user[i].addEventListener("click", (e) => {
-                        console.log(e.currentTarget);
+                        console.log(e.currentTarget.value);
+                        userMenu.hidden = false;
+
+                        fetch(/loadUserData, {
+                            method: 'POST',
+                            headers: {
+                                "Accept": 'application/json',
+                                "Content-Type": 'application/json'
+                              },
+                            body: JSON.stringify(e.currentTarget.value) 
+                        }).then(
+                            function (res) {
+                                const userData = res.json().then(
+                                    data => {
+                                        console.log(data);
+                                    }
+                                )
+                            }
+                        )
+
                     });
                 }
             });
@@ -37,36 +70,5 @@ function loadUserList () {
     )
 }
 
-// loads all user data and displays a table onto the admin page//
-function loadUserTable () {
-    const option = {
-        method: 'GET',
-    }
 
-    fetch("/getUserTable", option).then(
-        function (res) {
-          const result = res.json().then(
-            users => {
-                let newTable = document.createElement("table");
-                let headerRow = document.createElement("tr");
-                headerRow.innerHTML = "<th>User ID</th><th>First name</th><th>Last Name</th><th>Email</th><th>Username</th><th>Password</th><th>Admin</th>";
-                newTable.appendChild(headerRow);
-
-                for( let i=0; i < users.length; i++) {
-                    let newRow = document.createElement("tr");
-                    newRow.innerHTML += `<td>${users[i].userID}</td>`;
-                    newRow.innerHTML += `<td>${users[i].fname}</td>`;
-                    newRow.innerHTML += `<td>${users[i].lname}</td>`;
-                    newRow.innerHTML += `<td>${users[i].email}</td>`;
-                    newRow.innerHTML += `<td>${users[i].displayName}</td>`;
-                    newRow.innerHTML += `<td>${users[i].password}</td>`;
-                    newRow.innerHTML += `<td>${users[i].admin}</td>`;
-                    newTable.appendChild(newRow);
-                }
-
-                document.getElementById("content").appendChild(newTable);
-            });
-        }
-    )
-}
 
