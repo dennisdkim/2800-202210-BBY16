0f7b91d5fdeeded367cffd673d354c268bd718e6
Dennis Kim <dkim334@my.bcit.ca>
2022-05-13 10:12:14 -0700

    adjustments made to map radius

--- public/js/map.js
+++ public/js/map.js
@@ -20,7 +20,7 @@ function initMap() {
     location.long = pos.coords.longitude;
     map = new google.maps.Map(document.getElementById("map"), {
       center: { lat: location.lat, lng: location.long},
-      zoom: 15
+      zoom: 14
     });
 
     // creates a button re-center map to current location
@@ -36,6 +36,23 @@ function initMap() {
       );
     });
 
+    const radiusInput = document.createElement("input");
+    radiusInput.defaultValue = "2";
+    radiusInput.id = "radiusInput";
+    radiusInput.min = "1";
+    radiusInput.min = "10";
+    map.controls[google.maps.ControlPosition.RIGHT_TOP].push(radiusInput);
+    const currentRadius = new google.maps.Circle({
+      strokeColor: "#94bbdc",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#94bbdc",
+      fillOpacity: 0.3,
+      map,
+      center: { lat: location.lat, lng: location.long},
+      radius: Number(radiusInput.defaultValue) * 1000
+    });
+
     //creates a marker showing current location on map
     var marker = new google.maps.Marker({
       position: {lat: location.lat, lng: location.long},
@@ -49,7 +66,7 @@ function initMap() {
         strokeColor: '#ffffff',
       },
     });
-    getCoolzones(pos);
+    // getCoolzones(pos);
   }, error, options);
 }
 
