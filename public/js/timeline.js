69b3691079e2bc9c4330d8498b34f6dfb2520e0e
Bucksin <bucksin8@gmail.com>
2022-05-18 15:26:00 -0700

    added a post creation form in html in timeline.html. Completed the css for the form as well.

--- public/js/timeline.js
+++ public/js/timeline.js
@@ -1,4 +1,5 @@
 let postContent = document.getElementById("post-content-container");
+let postForm = document.getElementById("timeline-post-form-container");
 
 // shows/hides post-content-container. Input parameter 1 for showing, 0 for hiding. //
 function togglePostContent(input) {
@@ -6,5 +7,14 @@ function togglePostContent(input) {
         postContent.hidden = false;
     } else if (input == 0) {
         postContent.hidden = true;
+    }
+};
+
+function togglePostForm(input) {
+    
+    if (input == 1) {
+        postForm.hidden = false;
+    } else if (input == 0) {
+        postForm.hidden = true;
     }
 };
